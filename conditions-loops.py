fa28a3910b8e48b62e8d277c75e7608d8f6c2d82
Shaikh AtifaTanveer <145202138+ShaikhAtifa@users.noreply.github.com>
2026-02-22 23:26:51 +0530

    Implement multiple conditionals and loops examples
    Added multiple examples demonstrating conditional checks and loops in Python.

---
+++ conditions-loops.py
@@ -0,0 +1,45 @@
+ Multiple of 5 or not 
+num = int(input("enter number: "))  
+if num % 5 == 0:  
+print("multiple of 5")  
+else:  
+print("NOT a multiple of 5") 
+
+2. Odd or Even :
+num = int(input("enter number: "))  
+ 
+if num % 2 == 0:  
+    print("Even") 
+else:  
+    print("Odd") 
+ 
+ 
+ 
+#(Loops) 
+ 
+#1. Multiplication table of n:
+n= int(input("enter n: "))  
+i = 1  
+ 
+while i <= 10:  
+      print(i * n)  
+       i += 1 
+
+#2. Odd nums from 1 to 10, using continue 
+  
+#3. Count vowels 
+ 
+#4. Sum of first n Natural numbers 
+ 
+num = int(input("enter number: "))  
+ 
+if num % 2 == 0:  
+    print("Even") 
+else:  
+    print("Odd") 
+n = int(input("enter n: "))  
+i = 1  
+ 
+while i <= 10:  
+      print(i * n)  
+       i += 1 
