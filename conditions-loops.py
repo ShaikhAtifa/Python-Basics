4eb2643b221d76d3bf0a437b9dc4f00cba87b11d
Shaikh AtifaTanveer <145202138+ShaikhAtifa@users.noreply.github.com>
2026-02-22 23:29:40 +0530

    Refactor comments and fix formatting in conditions-loops.py
    Updated comments for clarity and corrected formatting issues in the code.

--- conditions-loops.py
+++ conditions-loops.py
@@ -1,4 +1,4 @@
- Multiple of 5 or not 
+# Multiple of 5 or not 
 num = int(input("enter number: "))  
 if num % 5 == 0:  
 print("multiple of 5")  
@@ -14,7 +14,7 @@ else:
     print("Odd") 
  
  
- 
+ #---------------------------------------------------------
 #(Loops) 
  
 #1. Multiplication table of n:
@@ -25,9 +25,21 @@ while i <= 10:
       print(i * n)  
        i += 1 
 
-#2. Odd nums from 1 to 10, using continue 
+#2. Odd nums from 1 to 10, using continue :
+i = 0  
+ 
+while(i < 10):  
+i += 1  
+if(i % 2 == 0):  
+continue;  
+print(i)  
   
-#3. Count vowels 
+#3. Count vowels :
+for ch in word:  
+    if (ch == 'a' or ch == 'a' or ch == 'a' or ch == 'a' or ch == 'a'):  
+        count += 1  
+ 
+print(f"vowel count = {count}") 
  
 #4. Sum of first n Natural numbers 
  
@@ -43,3 +55,21 @@ i = 1
 while i <= 10:  
       print(i * n)  
        i += 1 
+#---------------------------------------------------
+ #(Functions) 
+1. Factorial of N 
+# Factorial of N  
+n = int(input("enter n: "))  
+fact = 1  
+for i in range (1, n+1):  
+fact *= i  
+print("factorial = ", fact) 
+2. Largest of 3 numbers 
+def get_largest (a, b, c):  
+if (a > b and a > c): 
+return a 
+elif b > c: 
+return b 
+else: 
+return c 
+print (get_largest (3, 10, 5)) 
